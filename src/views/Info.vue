42e7b681cacd4072c835bcf484261e15e11ce140
hongwei <hwz57751314@qq.com>
2019-09-21 00:28:31 +0800

    debugger

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
     hello info component
+    <button type="button" @click="add()">添加</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Info',
-};
+  // @符号代表src目录，store.js的.js后缀可以省略
+  import store from '@/store';
+  export default {
+    name: 'Info',
+    store,
+    data() {
+      return {
+        msg: 'hello msg'
+      }
+    },
+    mounted (){
+      // 调试方法，window.vue这时候是info这个组件
+      window.vue = this
+    },
+    methods: {
+      add() {
+        console.log('add Event from info!');
+        debugger
+        store.commit('increase')
+      },
+      output(){
+        console.log("in output")
+      }
+    },
+  };
 </script>
 
 <style scoped>
